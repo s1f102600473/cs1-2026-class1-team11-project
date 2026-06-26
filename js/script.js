ca6ff2c31a4ab8e3c57b2b20df7739d5d1793599
s1f102600473 <s1f102600473@iniad.org>
2026-06-26 18:22:12 +0900

    目次の挙動を修正

--- js/script.js
+++ js/script.js
@@ -2,27 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const sections = document.querySelectorAll("main section[id]");
     const tocLinks = document.querySelectorAll(".toc-link");
 
-    function setActiveLink() {
-        let currentSectionId = "";
-
-        sections.forEach((section) => {
-            const sectionTop = section.offsetTop;
-
-            if (window.scrollY >= sectionTop - 120) {
-                currentSectionId = section.getAttribute("id");
-            }
-        });
-
-        tocLinks.forEach((link) => {
-            link.classList.remove("active");
-
-            const href = link.getAttribute("href");
-            if (href === `#${currentSectionId}`) {
-                link.classList.add("active");
-            }
-        });
-    }
-
     tocLinks.forEach((link) => {
         link.addEventListener("click", (e) => {
             e.preventDefault();
